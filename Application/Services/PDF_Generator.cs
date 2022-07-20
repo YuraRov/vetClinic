386a54ce06ee1e3db7d13131e52174575b885084
Anastasia Stetsenko <mubagu905@gmail.com>
2022-07-20 10:37:51 +0300

    first

---
+++ Application/Services/PDF_Generator.cs
@@ -0,0 +1,24 @@
+﻿using Core.Interfaces.Repositories;
+using Core.Interfaces.Services;
+using Core.Models;
+using Core.Paginator.Parameters;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Syncfusion.Drawing;
+using Syncfusion.Pdf;
+using Syncfusion.Pdf.Grid;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class PDF_Generator
+    {
+
+
+    }
+}
